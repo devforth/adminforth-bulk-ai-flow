04a148e6d1c4b9d29f5062e143dc2debd8014c47
yaroslav8765 <pechorkin2014@gmail.com>
2025-09-04 12:35:41 +0300

    chore: add comments for the types

--- types.ts
+++ types.ts
@@ -1,20 +1,52 @@
-import { ImageVisionAdapter, AdminUser, IAdminForth, StorageAdapter, ImageGenerationAdapter, CompletionAdapter } from "adminforth";
+import { ImageVisionAdapter, ImageGenerationAdapter, CompletionAdapter } from "adminforth";
 
 
 export interface PluginOptions {
+    /**
+     * Name of the action in three dots menu.
+     */
     actionName: string,
+
+     /**
+     * The adapter to use for scaning images and filling fields basing on the image content.
+     */
     visionAdapter?: ImageVisionAdapter,
+    
+    /**
+     * The adapter to use for text->text generation.
+     */
     textCompleteAdapter?: CompletionAdapter,
+
     /**
      * The adapter to use for image generation. 
      */
     imageGenerationAdapter?: ImageGenerationAdapter,
+    
+    /**
+     * List of fields that should be filled based on the image content analysis.
+     */
     fillFieldsFromImages?: Record<string, string>, // can analyze what is on image and fill fields, typical tasks "find dominant color", "describe what is on image", "clasify to one enum item, e.g. what is on image dog/cat/plant"
+
+    /**
+     * List of fields that should be filled based on the text.
+     */
     fillPlainFields?: Record<string, string>,
+
+    /**
+     * Number of tokens to generate (Only for text completion adapter). Default is 1000. 1 token ~= ¾ words
+     */
+    fillPlainFieldsMaxTokens?: number,
+
+    /**
+     * If you want to generate fields or images based on the image content attached images 
+     */
     attachFiles?: ({ record }: {
       record: any,
     }) => string[] | Promise<string[]>,
 
+    /**
+     * List of image fields, that should be filled.
+     */
     generateImages?: Record<
       string, {
         // can generate from images or just from another fields, e.g. "remove text from images", "improve image quality", "turn image into ghibli style"
@@ -43,6 +75,10 @@ export interface PluginOptions {
        */
       countToGenerate: number,
     }>,
+
+    /**
+     * Rate limits for each action.
+     */
     rateLimits?: {
       fillFieldsFromImages?: string, // e.g. 5/1d - 5 requests per day
       fillPlainFields?: string,
